b64eaec3eba965e3d1b388802377c84f10923c94
SaurabhOllato <saurabh.ollato@gmail.com>
2025-04-16 17:43:01 +0530

    wanted page

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    const phrases = ["artwork", "editorial", "motion"];
+    const phrases = ["content creation", "video $ photo editing", "motion"];
     let index = 0, char = 0, current = "", typing = true;
   
     function typeEffect() {
@@ -76,4 +76,15 @@ window.onload = function () {
     });
   });
   
+
+  
+  window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) { // Adjust this value based on when you want the effect to trigger
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+
 
